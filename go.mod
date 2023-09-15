5e46da01788244aa0ed0e5fe28406b465b56b231
Patricia Fargo <pfargo1701@gmail.com>
2023-09-14 23:20:27 -0500

    Init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/RealPatricia/pth
+
+go 1.21.1
---
+++ pth.go
@@ -0,0 +1,30 @@
+package pth
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+type Test[T, U any] struct {
+	Name      string
+	Input     T
+	Expecting U
+	Success   bool
+}
+
+func err[T any](expected, result T) string {
+	return fmt.Sprintf("\nExpecting:\t%v\nGot:\t%v", expected, result)
+}
+
+func Run[T, U any](tests []Test[T, U], f func(in T) U, t *testing.T) {
+	for _, test := range tests {
+		name := test.Name
+		t.Run(name, func(t *testing.T) {
+			result := f(test.Input)
+			if !reflect.DeepEqual(test.Expecting, result) {
+				t.Errorf(err(test.Expecting, result))
+			}
+		})
+	}
+}
